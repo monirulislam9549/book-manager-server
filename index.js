debbe938afcc7d46e20a7472759ef4553629b4cb
monirulislam9549 <monirulislam9549@gmail.com>
2023-05-16 15:06:22 +0600

    pushing

--- index.js
+++ index.js
@@ -63,6 +63,14 @@ async function run() {
       res.send(result);
     });
 
+    // delete
+    app.delete("/book/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
